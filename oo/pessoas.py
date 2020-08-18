84c801940cf3e066c12475bd6256796f33936437
R-3030casa <67975153+R-3030casa@users.noreply.github.com>
2020-08-18 15:17:35 -0300

    criado instacias e metodos para classe

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade = 57):
         self.idade = idade
         self.nome = nome
@@ -7,6 +8,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}' # imprimindo o id de self
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_class(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     moacir = Pessoa(nome='Moacir')
@@ -22,6 +30,20 @@ if __name__ == '__main__':
     print(ricardo.idade)
     for filho in ricardo.filhos:
         print(filho.nome)
+    # pode criar um atributo mesmo não estando no __init__
+    ricardo.sobrenome = 'Souza'
+    # pode deletar um atributo usando del
+    #del ricardo.filhos
+    ricardo.olhos = 1
+    print(ricardo.sobrenome)
+    print(ricardo.__dict__)
+    print(moacir.__dict__)
+    print(Pessoa.olhos)
+    print(ricardo.olhos)
+    print(moacir.olhos)
+    print(id(Pessoa.olhos), id(ricardo.olhos), id(moacir.olhos))
+    print(Pessoa.metodo_estatico(), ricardo.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_class(), ricardo.nome_e_atributos_de_class())
 
 
 
