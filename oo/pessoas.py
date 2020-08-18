f124bfa5eb2524cb5916d0f66e6e9258bb5fdd1d
R-3030casa <67975153+R-3030casa@users.noreply.github.com>
2020-08-18 13:57:53 -0300

    Criado objeto dinâmico do tipo pessoa

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,23 +1,28 @@
 class Pessoa:
-    def __init__(self, nome=None, idade = 57):
+    def __init__(self, *filhos, nome=None, idade = 57):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}' # imprimindo o id de self
 
 
 if __name__ == '__main__':
-    p = Pessoa('Moacir')
-    print(Pessoa.cumprimentar(p))
+    moacir = Pessoa(nome='Moacir')
+    ricardo = Pessoa(moacir, nome='ricardo')
     # em class não precisa passar o atributo como parâmetro
     #vamos exemplificar com o id do objeto
-    print(id(p))
-    print(p.cumprimentar())
     print(p.nome)
-    p.nome = 'Ricardo'
-    print(p.nome)
-    print(p.idade)
+    print(Pessoa.cumprimentar(ricardo))
+    print(id(ricardo))
+    print(ricardo.cumprimentar())
+    print(ricardo.nome)
+#    p.nome = 'Ricardo'
+#    print(p.nome)
+    print(ricardo.idade)
+    for filho in ricardo.filhos:
+        print(filho.nome)
 
 
 
