eccf0d985f1fe73dd64bd02df893e85a66342fb9
R-3030casa <67975153+R-3030casa@users.noreply.github.com>
2020-08-18 13:33:33 -0300

    atualizada a pasta oo

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,3 +1,23 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade = 57):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}' # imprimindo o id de self
+
+
+if __name__ == '__main__':
+    p = Pessoa('Moacir')
+    print(Pessoa.cumprimentar(p))
+    # em class não precisa passar o atributo como parâmetro
+    #vamos exemplificar com o id do objeto
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ricardo'
+    print(p.nome)
+    print(p.idade)
+
+
 
